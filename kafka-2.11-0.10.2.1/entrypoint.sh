d35a1efebad51fb646022213985409ff33344ae1
Andy <bwboy@163.com>
2018-06-13 21:07:32 +0800

    update kafka

--- kafka-2.11-0.10.2.1/entrypoint.sh
+++ kafka-2.11-0.10.2.1/entrypoint.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
+host_name =
 
 cat > /opt/kafka_2.11-0.10.2.1/config/server.properties <<EOF
 ##Server Basics##
 #broker.id=1 //不同服务器id不可重复
 delete.topic.enable=true
 ##Socket Server Settings##
-#listeners=PLAINTEXT://192.168.32.21:9092
-#advertised.listeners=PLAINTEXT://localhost:9092
+#listeners=PLAINTEXT://kafka01:9092
+#advertised.listeners=PLAINTEXT://kafka01:9092
 num.network.threads=3
 num.io.threads=8
 socket.send.buffer.bytes=102400
@@ -23,20 +24,16 @@ log.retention.hours=24
 log.segment.bytes=1073741824
 log.retention.check.interval.ms=300000
 ##Zookeeper集群地址##
-zookeeper.connect=192.168.32.21:2181,192.168.32.22:2181,192.168.32.23:2181
+#zookeeper.connect=192.168.32.21:2181,192.168.32.22:2181,192.168.32.23:2181
+zookeeper.connect=zookeeper01:2181
 ##Timeout in ms for connecting to zookeeper##
 zookeeper.connection.timeout.ms=6000
 EOF
 
 
 #broker.id
-echo "broker.id="$ID>>  /opt/kafka_2.11-0.10.2.1/config/server.properties
-echo "listeners=PLAINTEXT://\"$PLAINTEXT\" :9092" >> /opt/kafka_2.11-0.10.2.1/config/server.properties
-echo "advertised.listeners=PLAINTEXT://\"$PLAINTEXT\":9092" >> /opt/kafka_2.11-0.10.2.1/config/server.properties
-
-# zookeeper Server conigure
-if [ -n "$SERVERS" ]; then
-    printf '%s' "$SERVERS" | awk 'BEGIN { RS = "," }; { printf "server.%i=%s:2888:3888\n", NR, $0 }' >> /opt/kafka_2.11-0.10.2.1/config/server.properties
-fi
+echo "broker.id="$ID >>  /opt/kafka_2.11-0.10.2.1/config/server.properties
+echo "listeners=PLAINTEXT://$HOSTNAME:9092" >>  /opt/kafka_2.11-0.10.2.1/config/server.properties
+echo "advertised.listeners=PLAINTEXT://$HOSTNAME:9092" >>  /opt/kafka_2.11-0.10.2.1/config/server.properties
 
 exec "$@"
