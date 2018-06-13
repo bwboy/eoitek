e9b152f4da8964e158d29e0bea63faa3ee6b47f0
Andy <bwboy@163.com>
2018-06-13 22:41:15 +0800

    update kafka

--- kafka-2.11-0.10.2.1/entrypoint.sh
+++ kafka-2.11-0.10.2.1/entrypoint.sh
@@ -25,7 +25,7 @@ log.segment.bytes=1073741824
 log.retention.check.interval.ms=300000
 ##Zookeeper集群地址##
 #zookeeper.connect=192.168.32.21:2181,192.168.32.22:2181,192.168.32.23:2181
-zookeeper.connect=zookeeper01:2181
+#zookeeper.connect=zookeeper01:2181
 ##Timeout in ms for connecting to zookeeper##
 zookeeper.connection.timeout.ms=6000
 EOF
@@ -35,5 +35,6 @@ EOF
 echo "broker.id="$ID >>  /opt/kafka_2.11-0.10.2.1/config/server.properties
 echo "listeners=PLAINTEXT://$HOSTNAME:9092" >>  /opt/kafka_2.11-0.10.2.1/config/server.properties
 echo "advertised.listeners=PLAINTEXT://$HOSTNAME:9092" >>  /opt/kafka_2.11-0.10.2.1/config/server.properties
+echo "zookeeper.connect=$HOSTNAME:2181" >>  /opt/kafka_2.11-0.10.2.1/config/server.properties
 
 exec "$@"
