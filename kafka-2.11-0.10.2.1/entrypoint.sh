94fac2ad62e1ff60592ee2040749e0010e31a7aa
Andy <bwboy@163.com>
2018-06-13 20:38:46 +0800

    update kafka

--- kafka-2.11-0.10.2.1/entrypoint.sh
+++ kafka-2.11-0.10.2.1/entrypoint.sh
@@ -30,9 +30,9 @@ EOF
 
 
 #broker.id
-echo "broker.id=\"$ID !">>  /opt/kafka_2.11-0.10.2.1/config/server.properties
-echo "listeners=PLAINTEXT://\"$PLAINTEXT\" :9092!" >> /opt/kafka_2.11-0.10.2.1/config/server.properties
-echo "advertised.listeners=PLAINTEXT://\"$PLAINTEXT\":9092!" >> /opt/kafka_2.11-0.10.2.1/config/server.properties
+echo "broker.id=\"$ID\"">>  /opt/kafka_2.11-0.10.2.1/config/server.properties
+echo "listeners=PLAINTEXT://\"$PLAINTEXT\" :9092" >> /opt/kafka_2.11-0.10.2.1/config/server.properties
+echo "advertised.listeners=PLAINTEXT://\"$PLAINTEXT\":9092" >> /opt/kafka_2.11-0.10.2.1/config/server.properties
 
 # zookeeper Server conigure
 if [ -n "$SERVERS" ]; then
